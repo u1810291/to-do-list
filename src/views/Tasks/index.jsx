901a22f0a5c75c177a74ef0547efdec4f10f1669
u1810291 <o.rizakulov@student.inha.uz>
2021-05-23 03:58:21 +0500

    headers added

--- src/views/Tasks/index.jsx
+++ src/views/Tasks/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Tasks from '../../components/Tasks';
+import TasksHeader from '../../components/Headers/TasksHeader';
 import { Container } from './style';
 
 export default () => (
   <Container>
+    <TasksHeader />
     <Tasks />
   </Container>
 );
