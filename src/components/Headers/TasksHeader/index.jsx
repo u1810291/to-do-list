a351320b0bf12273921c87f0101a710db3af2ffb
u1810291 <o.rizakulov@student.inha.uz>
2021-05-23 12:38:31 +0500

    headers

--- src/components/Headers/TasksHeader/index.jsx
+++ src/components/Headers/TasksHeader/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { SearchInput } from '../../Forms/Inputs';
+import { PrimaryButton } from '../../Buttons';
 
 import { Container } from './style';
 
 export default () => (
   <Container>
-    Header9
+    <SearchInput placeholder="Search..." />
+    <PrimaryButton title="Add Task" size="large" />
   </Container>
 );
--- src/components/Headers/TasksHeader/style.js
+++ src/components/Headers/TasksHeader/style.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  `;
+  padding: 10px 0px 10px;
+  display:grid;
+  grid-gap: 40px;
+  grid-template-columns: auto 100px;
+`;
