0ce9744a467a1ab9e9509dbdfdb06c10d697b4c1
u1810291 <o.rizakulov@student.inha.uz>
2021-05-23 14:52:58 +0500

    add task

--- src/components/Headers/TasksHeader/index.jsx
+++ src/components/Headers/TasksHeader/index.jsx
@@ -3,10 +3,22 @@ import { SearchInput } from '../../Forms/Inputs';
 import { PrimaryButton } from '../../Buttons';
 
 import { Container } from './style';
+import { useShowModal } from '../../../hooks';
+import AddTask from './AddTask';
 
-export default () => (
-  <Container>
-    <SearchInput placeholder="Search..." />
-    <PrimaryButton title="Add Task" size="large" />
-  </Container>
-);
+export default () => {
+  const { showBlured } = useShowModal();
+  return (
+    <Container>
+      <SearchInput placeholder="Search..." />
+      <PrimaryButton
+        title="Add Task"
+        size="large"
+        onClick={() => showBlured({
+          title: 'Add Task',
+          body: () => <AddTask />
+        })}
+      />
+    </Container>
+  );
+};
