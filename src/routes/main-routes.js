7d8533484cdbe75ad07d43e1847b65f94bba8a12
u1810291 <o.rizakulov@student.inha.uz>
2021-06-07 14:49:09 +0500

    some changes

--- src/routes/main-routes.js
+++ src/routes/main-routes.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
-import SignIn from '../views/Auth/SignIn';
 import Dashboard from '../views/Tasks';
 
 export default [
@@ -9,14 +7,6 @@ export default [
     component: Dashboard,
     icon: 'tasks',
     title: 'Tasks',
-    children: []
-  },
-  {
-    exact: true,
-    path: '/signin',
-    component: SignIn,
-    icon: 'signin',
-    title: 'Sign In',
     children: []
   }
 ];
